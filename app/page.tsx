0a161a930c6f01095ec2ab2c3aefbcd3485ba24b
l11040 <l1104033837@gmail.com>
2025-03-17 14:56:47 +0900

    [Init]

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="h-svh bg-blue-500"></div>
+      <div className="h-svh bg-red-500"></div>
+      <div className="h-svh bg-green-500"></div>
+    </main>
+  );
+}
