111bdf35ec47c7c60044751f2c2aebc9eaf184fa
l11040 <l1104033837@gmail.com>
2025-03-25 13:46:20 +0900

    [Update] 화살표 버튼 수정

--- components/home/ButtonWithArrow.tsx
+++ components/home/ButtonWithArrow.tsx
@@ -1,24 +1,33 @@
 import { RightArrowIcon } from "@/utils/svgr";
 import { ReactNode, useState } from "react";
+import clsx from "clsx";
 
 interface ButtonWithArrowProps {
   children: ReactNode;
+  className?: string;
 }
 
-export default function ButtonWithArrow({ children }: ButtonWithArrowProps) {
+export default function ButtonWithArrow({
+  children,
+  className = "",
+}: ButtonWithArrowProps) {
   const [hovered, setHovered] = useState(false);
 
   return (
     <button
-      className="px-5 py-2 bg-signature text-reverseForground rounded-xl flex relative overflow-hidden cursor-pointer shadow-md"
+      className={clsx(
+        "px-4 py-2 bg-signature/80 text-reverseForground rounded-lg flex relative overflow-hidden cursor-pointer w-fit",
+        className
+      )}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
       {children}
       <RightArrowIcon
-        className={`absolute size-3 left-full top-1/2 transform -translate-y-1/2 transition-all duration-300 ease-in-out ${
+        className={clsx(
+          "absolute size-3 left-full top-1/2 transform -translate-y-1/2 transition-all duration-300 ease-in-out",
           hovered ? "opacity-100 -translate-x-4" : "opacity-0"
-        }`}
+        )}
       />
     </button>
   );
