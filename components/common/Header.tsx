883e221790bf100f82f53a192fcea91c94101423
l11040 <l1104033837@gmail.com>
2025-03-28 20:03:50 +0900

    [Update] 헤더 Products 링크 이동 추가

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -2,13 +2,14 @@
 
 import { Menu, X } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { useState, useRef, useEffect } from "react";
 
 const MENU_ITEMS = [
-  { en: "About Us", ko: "회사소개" },
-  { en: "Products", ko: "생산제품" },
-  { en: "Production Facilities", ko: "생산설비" },
-  { en: "Inquiry", ko: "문의" }
+  { en: "About Us", ko: "회사소개", link: "/" },
+  { en: "Products", ko: "생산제품", link: "/products" },
+  { en: "Production Facilities", ko: "생산설비", link: "/" },
+  { en: "Inquiry", ko: "문의", link: "/" }
 ];
 
 export default function Header() {
@@ -26,23 +27,36 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="fixed top-0 z-10 h-fit min-h-16 w-full bg-white/30 backdrop-blur-md md:h-16">
-      <nav className="flex h-full w-full items-center justify-between px-4 md:justify-center">
-        <Image src="/logo.png" alt="" width={40} height={40} className="mr-6" />
+    <header className="m-h-16 border-background2 fixed top-0 z-10 w-full border-b-2 bg-white/60 backdrop-blur-md">
+      <nav className="flex h-full w-full items-center justify-between px-4 py-3 md:justify-center">
+        <Link href={"/"}>
+          <Image
+            src="/logo.png"
+            alt=""
+            width={40}
+            height={40}
+            className="mr-6"
+          />
+        </Link>
+        {/* Desktop 메뉴 */}
         <ul className="hidden space-x-6 md:flex">
           {MENU_ITEMS.map((item, index) => (
             <li
               key={index}
-              className="text-foreground cursor-pointer text-center text-base whitespace-nowrap transition-all duration-200"
               onMouseEnter={() => setHoveredIndex(index)}
               onMouseLeave={() => setHoveredIndex(null)}
               style={{
                 minWidth: maxWidths[index] ? `${maxWidths[index]}px` : "auto"
               }}
             >
-              <span ref={(el) => setRefs(el, index)}>
-                {hoveredIndex === index ? item.ko : item.en}
-              </span>
+              <Link
+                href={item.link}
+                className="text-foreground block text-center text-base whitespace-nowrap transition-all duration-200"
+              >
+                <span ref={(el) => setRefs(el, index)}>
+                  {hoveredIndex === index ? item.ko : item.en}
+                </span>
+              </Link>
             </li>
           ))}
         </ul>
@@ -54,6 +68,8 @@ export default function Header() {
           {menuOpen ? <X size={24} /> : <Menu size={24} />}
         </button>
       </nav>
+
+      {/* Mobile 메뉴 */}
       <nav
         className={`overflow-hidden transition-all duration-300 ease-in-out md:hidden ${
           menuOpen ? "max-h-96 py-4" : "max-h-0 py-0"
@@ -63,9 +79,15 @@ export default function Header() {
           {MENU_ITEMS.map((item, index) => (
             <li
               key={index}
-              className="text-foreground w-full cursor-pointer py-2 text-center text-base"
+              className="text-foreground w-full text-center text-base"
             >
-              {item.ko}
+              <Link
+                href={item.link}
+                onClick={() => setMenuOpen(false)}
+                className="block py-2"
+              >
+                {item.ko}
+              </Link>
             </li>
           ))}
         </ul>
