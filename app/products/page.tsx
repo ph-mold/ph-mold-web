c08cf528d265f3ee7cd25b3b00421beb8c7ef88a
l11040 <l1104033837@gmail.com>
2025-03-31 14:47:40 +0900

    [Update]

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -42,52 +42,62 @@ export default function Products() {
       </Typography>
       <div className="border-background2 scrollbar-hide sticky top-16 z-9 h-12 w-full overflow-x-scroll overflow-y-hidden border-b-2 bg-white/60 backdrop-blur-md">
         <Tab
-          className="px-4 md:px-10"
+          className="mx-auto w-full max-w-[1280px] px-4 md:px-10"
           onChange={handleTabSelect}
           tabs={TAB_ITEMS}
         />
       </div>
-      <div className="mx-auto max-w-[1280px]">
-        <div className="border-background2 scrollbar-hide h-fit overflow-x-scroll border-b-2 py-2 sm:py-4">
-          <ul className="flex h-fit flex-row px-4 md:px-10">
-            <li className="flex h-fit w-fit flex-col text-center">
-              <div className="size-24 flex-none sm:size-36">
-                <Image src={"/sample1.png"} alt={""} width={240} height={240} />
-              </div>
-              <p className="text-sm">PP 주사기</p>
-            </li>
-            <li className="flex h-fit w-fit flex-col text-center">
-              <div className="size-24 flex-none sm:size-36">
-                <Image src={"/sample1.png"} alt={""} width={240} height={240} />
-              </div>
-              <p className="text-sm">PP 주사기</p>
-            </li>
-            <li className="flex h-fit w-fit flex-col text-center">
-              <div className="size-24 flex-none sm:size-36">
-                <Image src={"/sample1.png"} alt={""} width={240} height={240} />
-              </div>
-              <p className="text-sm">PP 주사기</p>
-            </li>
-            <li className="flex h-fit w-fit flex-col text-center">
-              <div className="size-24 flex-none sm:size-36">
-                <Image src={"/sample1.png"} alt={""} width={240} height={240} />
-              </div>
-              <p className="text-sm">PP 주사기</p>
-            </li>
-            <li className="flex h-fit w-fit flex-col text-center">
-              <div className="size-24 flex-none sm:size-36">
-                <Image src={"/sample1.png"} alt={""} width={240} height={240} />
-              </div>
-              <p className="text-sm">PP 주사기</p>
-            </li>
-            <li className="flex h-fit w-fit flex-col text-center">
-              <div className="size-24 flex-none sm:size-36">
-                <Image src={"/sample1.png"} alt={""} width={240} height={240} />
-              </div>
-              <p className="text-sm">PP 주사기</p>
-            </li>
-          </ul>
-        </div>
+      <div className="border-background2 scrollbar-hide mx-auto w-full max-w-[1280px] overflow-x-scroll overflow-y-hidden border-b-2 px-4 md:px-10">
+        <Tab
+          className="py-2"
+          showIndicator={false}
+          tabClassName="flex-col !text-sm !p-0"
+          activeTabClassName="!font-medium !text-foreground2"
+          tabs={[
+            {
+              label: "PP 주사기",
+              value: "s",
+              icon: (
+                <div className="size-24 flex-none sm:size-32">
+                  <Image
+                    src={"/sample1.png"}
+                    alt={""}
+                    width={240}
+                    height={240}
+                  />
+                </div>
+              )
+            },
+            {
+              label: "PP 주사기",
+              value: "s1",
+              icon: (
+                <div className="size-24 flex-none sm:size-32">
+                  <Image
+                    src={"/sample1.png"}
+                    alt={""}
+                    width={240}
+                    height={240}
+                  />
+                </div>
+              )
+            },
+            {
+              label: "PP 주사기",
+              value: "s2",
+              icon: (
+                <div className="size-24 flex-none sm:size-32">
+                  <Image
+                    src={"/sample1.png"}
+                    alt={""}
+                    width={240}
+                    height={240}
+                  />
+                </div>
+              )
+            }
+          ]}
+        />
       </div>
       <div className="h-[2000px]"> </div>
     </div>
--- components/common/Tab.tsx
+++ components/common/Tab.tsx
@@ -64,7 +64,7 @@ export default function Tab({
     };
   }, [activeTab, showIndicator, tabs]);
   return (
-    <div className={clsx("relative w-fit", className)}>
+    <div className={clsx("relative", className)}>
       <ul className="flex gap-3">
         {tabs.map((tab) => {
           const isActive = activeTab === tab.value;
