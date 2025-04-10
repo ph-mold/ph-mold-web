ac19045c0e2446df33f627e7c8e1bd45b53fcde0
l11040 <l1104033837@gmail.com>
2025-04-10 15:22:53 +0900

    [Chore]

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,15 +1,12 @@
-// lib/fetcher.ts
-
 export const fetcher = async <T>(
   url: string,
   options?: RequestInit & { cacheType?: "no-store" | "force-cache" | "default" }
-): Promise<T> => {
+): Promise<T | undefined> => {
   const cache = options?.cacheType ?? "no-store";
   const res = await fetch(url, { ...options, cache });
 
   if (!res.ok) {
-    console.error(`Fetch failed: ${url}`);
-    throw new Error("Fetch error");
+    return;
   }
 
   return res.json();
