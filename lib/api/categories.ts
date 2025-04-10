ba7e5216da06f7711d753f20db64d03047ab498a
l11040 <l1104033837@gmail.com>
2025-04-10 15:20:55 +0900

    [Add] 자식 카테고리 조회 추가

--- lib/api/categories.ts
+++ lib/api/categories.ts
@@ -1,7 +1,15 @@
 import { API } from "@/lib/constants/api";
 import { fetcher } from "../fetcher";
-import { IGetRootCategory } from "@/types/api/category";
+import { IGetCategory } from "@/types/api/category";
 
-export async function getRootCategory(): Promise<IGetRootCategory[]> {
+export async function getRootCategory(): Promise<IGetCategory[] | undefined> {
   return fetcher(API.CATEGORIES.GET, { cache: "force-cache" });
+}
+
+export async function getCategoryByParentKey(
+  parentKey: string
+): Promise<IGetCategory[] | undefined> {
+  return fetcher(API.CATEGORIES.GET_BY_PARENT_KEY(parentKey), {
+    cache: "force-cache"
+  });
 }
--- lib/constants/api.ts
+++ lib/constants/api.ts
@@ -2,7 +2,9 @@ export const MOCK_URL = "/api";
 
 export const API = {
   CATEGORIES: {
-    GET: `${process.env.API_BASE_URL}/categories`
+    GET: `${process.env.API_BASE_URL}/categories`,
+    GET_BY_PARENT_KEY: (key: string) =>
+      `${process.env.API_BASE_URL}/categories/${key}`
   },
   PRODUCT_CATEGORIES: {
     GET: `${MOCK_URL}/product-categories`
