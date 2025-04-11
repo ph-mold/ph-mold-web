bb3efd182b5d01ec9bc5d6c1341afa61301c8a5f
l11040 <l1104033837@gmail.com>
2025-04-11 11:03:18 +0900

    [Fix]

--- lib/constants/api.ts
+++ lib/constants/api.ts
@@ -2,9 +2,9 @@ export const MOCK_URL = "/api";
 
 export const API = {
   CATEGORIES: {
-    GET: `${process.env.API_BASE_URL}/categories`,
+    GET: `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories`,
     GET_BY_PARENT_KEY: (key: string) =>
-      `${process.env.API_BASE_URL}/categories/${key}`
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/${key}`
   },
   PRODUCT_CATEGORIES: {
     GET: `${MOCK_URL}/product-categories`
