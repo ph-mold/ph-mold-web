010e8f342729a16ad4d4b666e6745d4abe79dcd6
l11040 <l1104033837@gmail.com>
2025-04-14 21:48:45 +0900

    [Update] 실제 API 요청에 맞는 제품 grid 컴포넌트 구성

--- components/products/ProductGrid.tsx
+++ components/products/ProductGrid.tsx
@@ -1,34 +1,49 @@
 "use client";
 
 import { imageLoader } from "@/lib/imageLoader";
+import { IGetProduct } from "@/types/api/product";
+import { ImageOff } from "lucide-react";
 import Image from "next/image";
 
-const sampleItems = Array.from({ length: 20 }).map((_, i) => ({
-  id: `PMY-${i.toString().padStart(3, "0")}`,
-  name: "1ml 주사기 / 시린지 / 백색",
-  size: "18.2 × 13 × 83",
-  imageUrl: "/sample1.png"
-}));
+interface Props {
+  products?: IGetProduct[];
+}
 
-export default function ProductGrid() {
+export default function ProductGrid({ products }: Props) {
   return (
     <div className="my-8 grid grid-cols-2 gap-3 space-y-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-      {sampleItems.map((item) => (
-        <div key={item.id} className="flex flex-col gap-2">
+      {products?.map((item) => (
+        <div key={item.id} className="relative flex flex-col gap-2">
+          <div className="text-foreground2 absolute top-1 left-2 text-xs font-semibold">
+            {item.material}
+          </div>
           <div className="bg-background2 flex aspect-square items-center justify-center overflow-hidden rounded-lg">
-            <Image
-              loader={imageLoader}
-              src={item.imageUrl}
-              alt={item.id}
-              width={240}
-              height={240}
-              className="h-full w-full object-contain"
-            />
+            {item.thumbnailImageUrl ? (
+              <Image
+                loader={imageLoader}
+                src={item.thumbnailImageUrl}
+                alt={item.name}
+                width={240}
+                height={240}
+                className="h-full w-full object-contain"
+              />
+            ) : (
+              <ImageOff className="stroke-signature/30 size-[30%] stroke-[1.5] object-contain" />
+            )}
           </div>
           <div className="space-y-1">
-            <p className="text-sm font-bold">{item.id}</p>
-            <p className="text-sm">{item.name}</p>
-            <p className="text-foreground2 text-xs">{item.size}</p>
+            <p className="text-foreground2 text-xs">{item.code}</p>
+            <p className="text-sm font-bold">{item.name}</p>
+            <div className="flex flex-wrap space-y-1 space-x-1 py-1">
+              {item.tags.map((tag) => (
+                <p
+                  key={tag.key}
+                  className="bg-background2 text-signature h-6 rounded-md px-1 py-[1px] text-sm text-nowrap"
+                >
+                  {tag.name}
+                </p>
+              ))}
+            </div>
           </div>
         </div>
       ))}
