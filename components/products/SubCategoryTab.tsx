bea0d04f8ccc0a9c71858f8a7b290f5e1dca2602
l11040 <l1104033837@gmail.com>
2025-04-14 21:19:44 +0900

    [Update] 서브 카테고리 쿼리 사용하도록 수정

--- components/products/SubCategoryTab.tsx
+++ components/products/SubCategoryTab.tsx
@@ -5,6 +5,7 @@ import Tab from "../common/Tab";
 import { IGetCategory } from "@/types/api/category";
 import { mapCategoriesToSubTabItems } from "@/lib/mapper/categoryToTabItem";
 import { imageLoader } from "@/lib/imageLoader";
+import { useTabNavigation } from "@/hooks/useTabNavigation";
 
 interface SubCategoryTabProps {
   subTabs: IGetCategory[];
@@ -33,10 +34,20 @@ export default function SubCategoryTab({ subTabs }: SubCategoryTabProps) {
       </div>
     )
   });
+
+  const { activeTab, handleTabClick } = useTabNavigation({
+    tabs: tabItems,
+    queryKey: "sub",
+    mode: "query",
+    syncParams: ["tab"]
+  });
+
   return (
     <div className="border-background2 scrollbar-hide w-full overflow-x-scroll overflow-y-hidden border-b-2">
       <Tab
         className="pt-2 pb-3"
+        activeTab={activeTab}
+        onChange={handleTabClick}
         showIndicator={false}
         tabClassName="flex-col !text-sm !p-0"
         activeTabClassName="!font-medium !text-foreground2"
