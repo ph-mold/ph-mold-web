28e55dc2a742d797bbb721bd6bca14f0ed5ce314
l11040 <l1104033837@gmail.com>
2025-04-15 11:23:17 +0900

    [Fix] 빌드 시 경로 문제 수정

--- lib/constants/api.ts
+++ lib/constants/api.ts
@@ -2,12 +2,11 @@ export const MOCK_URL = "/api";
 
 export const API = {
   CATEGORIES: {
-    GET: `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories`,
-    GET_BY_PARENT_KEY: (key: string) =>
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/${key}`
+    GET: `/categories`,
+    GET_BY_PARENT_KEY: `/categories`
   },
   PRODUCTS: {
-    GET_BY_CATEGORY: `${process.env.NEXT_PUBLIC_API_BASE_URL}/products`
+    GET_BY_CATEGORY: `/products`
   },
   PRODUCT_CATEGORIES: {
     GET: `${MOCK_URL}/product-categories`
