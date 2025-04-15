56e5537f1a6fbf56416412456e7aab2c4f969614
l11040 <l1104033837@gmail.com>
2025-04-15 14:34:41 +0900

    [Update] NotFound 페이지 구성

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,12 @@
-import Link from "next/link";
+import { SearchX } from "lucide-react";
 
 export default function NotFound() {
   return (
-    <div className="mt-16">
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="mt-16 flex min-h-[300px] items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <SearchX className="stroke-foreground2 size-20 stroke-[1px]" />
+        <p className="text-foreground2 text-lg">페이지를 찾을 수 없습니다.</p>
+      </div>
     </div>
   );
 }
