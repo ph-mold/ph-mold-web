f31755af79d5bd18e1c8d45481bcf328d7acd6e2
l11040 <l1104033837@gmail.com>
2025-04-17 14:08:20 +0900

    [Add] 제품 고정 헤더 목업 추가

---
+++ components/Product/ProductStickyBar.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Button from "../common/Button";
+
+export default function ProductStickyBar() {
+  return (
+    <div className="border-background2 sticky top-16 z-9 w-full border-b-2 bg-white/80 backdrop-blur-md">
+      <div className="mx-auto flex w-full max-w-[1280px] flex-row items-center justify-between px-4 py-2 md:px-10">
+        <div className="flex flex-col">
+          <p className="text-foreground2 text-xs">SYRPP001001</p>
+          <p className="text-sm font-bold">1ml PP 주사기</p>
+        </div>
+        <Button className="w-24" size="small">
+          샘플요청
+        </Button>
+      </div>
+    </div>
+  );
+}
