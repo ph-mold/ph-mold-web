77089cfda06222833a7cbf2a10941b325f60bbc6
l11040 <l1104033837@gmail.com>
2025-04-17 14:58:25 +0900

    [Add] 제품 정보 패널 목업 추가

--- app/product/[productKey]/page.tsx
+++ app/product/[productKey]/page.tsx
@@ -1,4 +1,5 @@
 import ProductImageGallery from "@/components/product/ProductImageGallery";
+import ProductInfoPanel from "@/components/product/ProductInfoPanel";
 
 interface Props {
   params: Promise<{ productKey: string }>;
@@ -7,9 +8,10 @@ interface Props {
 export default async function Product({ params }: Props) {
   const { productKey } = await params;
   return (
-    <div className="mx-auto w-full max-w-[1280px] px-4 md:px-10">
-      <div className="mt-4 grid grid-cols-1 gap-12 sm:grid-cols-2">
+    <div className="mx-auto w-full max-w-[1080px] px-4 md:px-10">
+      <div className="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-4 md:gap-12">
         <ProductImageGallery />
+        <ProductInfoPanel />
       </div>
       <div className="h-[2000px]">{productKey}</div>
     </div>
