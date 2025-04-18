89787bbfee087744538bda84d68a3fbd9734bdaa
l11040 <l1104033837@gmail.com>
2025-04-18 17:04:51 +0900

    [Update] 제품 상세 메타데이터 추가

--- app/product/[productKey]/page.tsx
+++ app/product/[productKey]/page.tsx
@@ -1,8 +1,31 @@
 import ProductImageGallery from "@/components/product/ProductImageGallery";
 import ProductInfoPanel from "@/components/product/ProductInfoPanel";
+import { getProductSummaryByKey } from "@/lib/api/products";
 
 interface Props {
   params: Promise<{ productKey: string }>;
+}
+
+export async function generateMetadata({ params }: Props) {
+  const { productKey } = await params;
+  const summary = await getProductSummaryByKey(productKey);
+  const originalImageUrl = `${process.env.NEXT_PUBLIC_FILE_SERVER_BASE_URL}${summary.thumbnailImageUrl}`;
+  return {
+    title: summary.name,
+    description: `${summary.name} 제품의 상세 정보`,
+    openGraph: {
+      title: summary.name,
+      description: `${summary.name} 제품을 확인해보세요.`,
+      images: [
+        {
+          url: originalImageUrl,
+          width: 400,
+          height: 400,
+          alt: `${summary.name} 썸네일`
+        }
+      ]
+    }
+  };
 }
 
 export default async function Product({ params }: Props) {
