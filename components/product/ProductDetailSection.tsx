52e6816ddc48a162a1157c8eee56782fbc5e537b
l11040 <l1104033837@gmail.com>
2025-04-19 23:29:35 +0900

    [Update] 제품 이미지 실제 데이터를 이용하도록 수정

--- components/product/ProductDetailSection.tsx
+++ components/product/ProductDetailSection.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import {
+  GET_PRODUCT_IMAGES_BY_KEY,
   GET_PRODUCT_INFO_BY_KEY,
+  getProductImagesByKey,
   getProductInfoByKey
 } from "@/lib/api/products";
-import { IGetProductInfo } from "@/types/api/product";
+import { IGetProductImage, IGetProductInfo } from "@/types/api/product";
 import { notFound } from "next/navigation";
 import useSWR from "swr";
 import ProductImageGallery from "./ProductImageGallery";
@@ -12,12 +14,22 @@ import ProductInfoPanel from "./ProductInfoPanel";
 import WithSkeleton from "../common/WithSkeleton";
 import ProductInfoPanelSkeleton from "./ProductInfoPanel.skeleton";
 import { useStickyButtonRef } from "@/context/StickyButtonContext";
+import ProductImageGallerySkeleton from "./ProductImageGallery.skeleton";
 
 interface Props {
   productKey: string;
 }
 
 export default function ProductDetailSection({ productKey }: Props) {
+  const {
+    data: images,
+    isLoading: isImagesLoading,
+    error: imagesError
+  } = useSWR<IGetProductImage[] | undefined>(
+    [GET_PRODUCT_IMAGES_BY_KEY, productKey],
+    () => getProductImagesByKey(productKey)
+  );
+
   const {
     data: info,
     isLoading: isInfoLoading,
@@ -27,7 +39,7 @@ export default function ProductDetailSection({ productKey }: Props) {
     () => getProductInfoByKey(productKey)
   );
 
-  if (infoError) {
+  if (infoError || imagesError) {
     notFound();
   }
   const ref = useStickyButtonRef();
@@ -35,7 +47,12 @@ export default function ProductDetailSection({ productKey }: Props) {
   return (
     <div className="mx-auto w-full max-w-[1080px] px-4 md:px-10">
       <div className="my-4 grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-4 md:gap-12">
-        <ProductImageGallery />
+        <WithSkeleton
+          isLoading={isImagesLoading}
+          skeleton={<ProductImageGallerySkeleton />}
+        >
+          {images && <ProductImageGallery images={images} />}
+        </WithSkeleton>
         <div className="h-fit">
           <WithSkeleton
             isLoading={isInfoLoading}
@@ -46,7 +63,7 @@ export default function ProductDetailSection({ productKey }: Props) {
           <span ref={ref} />
         </div>
       </div>
-      <div className="h-[2000px]">{productKey}</div>
+      <div className="h-[2000px]" />
     </div>
   );
 }
--- components/product/ProductImageGallery.tsx
+++ components/product/ProductImageGallery.tsx
@@ -5,63 +5,72 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import type { Swiper as SwiperClass } from "swiper/types";
 import Image from "next/image";
 import Button from "@/components/common/Button";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, ImageOff } from "lucide-react";
 import { imageLoader } from "@/lib/imageLoader";
 import "swiper/css";
+import { IGetProductImage } from "@/types/api/product";
 
-const thumbnails = Array.from({ length: 4 }, (_, i) => `/sample${i + 1}.png`);
+interface Props {
+  images: IGetProductImage[];
+}
 
-export default function ProductImageGallery() {
+export default function ProductImageGallery({ images }: Props) {
   const swiperRef = useRef<SwiperClass | null>(null);
   const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
-    <div className="flex flex-col gap-4">
-      <div className="bg-background2 relative aspect-square w-full overflow-hidden rounded-lg">
-        <Swiper
-          loop={true}
-          onSwiper={(swiper) => {
-            swiperRef.current = swiper;
-          }}
-          onSlideChange={(swiper) => setCurrentIndex(swiper.realIndex)}
-          spaceBetween={16}
-          slidesPerView={1}
-          className="h-full w-full"
-        >
-          {thumbnails.map((src, idx) => (
-            <SwiperSlide key={idx}>
-              <Image
-                loader={imageLoader}
-                src={src}
-                alt={`image-${idx}`}
-                width={600}
-                height={600}
-                className="h-full w-full object-contain"
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+    <div className="flex flex-col gap-2">
+      <div className="bg-background2 relative flex aspect-square w-full items-center justify-center overflow-hidden rounded-lg">
+        {images.length === 0 ? (
+          <ImageOff className="stroke-signature size-[30%] stroke-[1.5] object-contain opacity-30" />
+        ) : (
+          <>
+            <Swiper
+              loop={true}
+              onSwiper={(swiper) => {
+                swiperRef.current = swiper;
+              }}
+              onSlideChange={(swiper) => setCurrentIndex(swiper.realIndex)}
+              spaceBetween={16}
+              slidesPerView={1}
+              className="h-full w-full"
+            >
+              {images.map((image, idx) => (
+                <SwiperSlide key={idx}>
+                  <Image
+                    loader={imageLoader}
+                    src={image.url}
+                    alt={`image-${image.id}`}
+                    width={600}
+                    height={600}
+                    className="h-full w-full object-contain"
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
 
-        <Button
-          onClick={() => swiperRef.current?.slidePrev()}
-          variant="text"
-          size="large"
-          className="absolute top-1/2 left-2 z-1 -translate-y-1/2 !p-3"
-        >
-          <ChevronLeft />
-        </Button>
-        <Button
-          onClick={() => swiperRef.current?.slideNext()}
-          variant="text"
-          size="large"
-          className="absolute top-1/2 right-2 z-1 -translate-y-1/2 !p-3"
-        >
-          <ChevronRight />
-        </Button>
+            <Button
+              onClick={() => swiperRef.current?.slidePrev()}
+              variant="text"
+              size="large"
+              className="absolute top-1/2 left-2 z-1 -translate-y-1/2 !p-3"
+            >
+              <ChevronLeft />
+            </Button>
+            <Button
+              onClick={() => swiperRef.current?.slideNext()}
+              variant="text"
+              size="large"
+              className="absolute top-1/2 right-2 z-1 -translate-y-1/2 !p-3"
+            >
+              <ChevronRight />
+            </Button>
+          </>
+        )}
       </div>
 
       <div className="grid grid-cols-5 gap-2">
-        {thumbnails.map((src, idx) => (
+        {images.map((image, idx) => (
           <Button
             key={idx}
             onClick={() => swiperRef.current?.slideToLoop(idx)}
@@ -72,8 +81,8 @@ export default function ProductImageGallery() {
           >
             <Image
               loader={imageLoader}
-              src={src}
-              alt={`thumb-${idx}`}
+              src={image.url}
+              alt={`thumb-${image.id}`}
               width={120}
               height={120}
               className="h-full w-full object-contain"
