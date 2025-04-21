ee3699b517ba1530c1bc973ddd5918542dc69af2
l11040 <l1104033837@gmail.com>
2025-04-21 16:54:35 +0900

    [Add] 제품 상세정보 스켈레톤 추가

--- components/product/ProductDetailSection.tsx
+++ components/product/ProductDetailSection.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ProductDetail from "./ProductDetail";
 import ProductImageGallery from "./ProductImageGallery";
 import ProductInfoPanel from "./ProductInfoPanel";
 import { useStickyButtonRef } from "@/context/StickyButtonContext";
@@ -12,16 +13,15 @@ export default function ProductDetailSection({ productKey }: Props) {
   const ref = useStickyButtonRef();
 
   return (
-    <div className="mx-auto w-full max-w-[1080px] px-4 md:px-10">
+    <div className="mx-auto mb-10 flex w-full max-w-[1080px] flex-col gap-10 px-4 md:px-10">
       <div
         ref={ref}
         className="my-4 grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-4 md:gap-12"
       >
         <ProductImageGallery productKey={productKey} />
-        <div className="h-fit">
-          <ProductInfoPanel productKey={productKey} />
-        </div>
+        <ProductInfoPanel productKey={productKey} />
       </div>
+      <ProductDetail productKey={productKey} />
     </div>
   );
 }
